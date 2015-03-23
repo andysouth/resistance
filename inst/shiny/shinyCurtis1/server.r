3365f751e40d27ffbd5c330622350be935a84a2d
AndySouth <southandy@gmail.com>
2015-03-23 22:52:30 +0000

    got shiny app working and added more inputs

--- inst/shiny/shinyCurtis1/server.r
+++ inst/shiny/shinyCurtis1/server.r
@@ -10,11 +10,18 @@ shinyServer(function(input, output) {
   
   output$plot <- renderPlot({
     
-    #p <- 
-      
-    resistSimple( input$P_1, input$P_2, input$h.RS1_00, input$h.RS2_00 )
+
+    cat("running resistSimple with these inputs:", input$P_1, input$P_2*input$P_1, input$h.RS1_00, input$h.RS2_00,"\n")
     
-    #print(p)
+    resistSimple( P_1 = input$P_1, 
+                  P_2 = input$P_2*input$P_1, #i need to put a limit of 1 on this
+                  h.RS1_00 = input$h.RS1_00, 
+                  h.RS2_00 = input$h.RS2_00,
+                  phi.SS1_A0 = input$phi.SS1_A0,
+                  phi.SS2_0B = input$phi.SS2_0B,
+                  s.RR1_A0 = input$s.RR1_A0,
+                  s.RR2_0B = input$s.RR2_0B )
+
     
   })
   
--- inst/shiny/shinyCurtis1/ui.r
+++ inst/shiny/shinyCurtis1/ui.r
@@ -13,16 +13,26 @@ shinyUI(fluidPage(
   hr(),
   
   fluidRow(
-    column(3,
-           h4("Starting frequency of resistance"),
-           numericInput("P_1", "locus1:", 0.05, min = 0.01, max = 0.1),
-           numericInput("P_2", "locus2:", 0.05, min = 0.01, max = 0.1)
+    column(2,
+           h5("Starting frequency of resistance"),
+           numericInput("P_1", "locus1:", 0.05, min = 0.01, max = 0.1, step = 0.01),
+           numericInput("P_2", "locus2 - proportion of locus1:", 0.1, min = 0.01, max = 100, step = 0.01)
     ),
-    column(4, offset = 0,
-           h4("Dominance of resistance"),
-           numericInput("h.RS1_00", "locus1:", 0.2, min = 0, max = 1),
-           numericInput("h.RS2_00", "locus2:", 0.2, min = 0, max = 1)
+    column(2, offset = 0,
+           h5("Dominance of resistance"),
+           numericInput("h.RS1_00", "locus1:", 0.2, min = 0, max = 1, step = 0.05),
+           numericInput("h.RS2_00", "locus2:", 0.2, min = 0, max = 1, step = 0.05)
+    ),
+    column(2, offset = 0,
+           h5("Fitness of susceptibles(SS) exposed"),
+           numericInput("phi.SS1_A0", "locus1:", 0.05, min = 0, max = 0.4, step = 0.05),
+           numericInput("phi.SS2_0B", "locus2:", 0.05, min = 0, max = 0.4, step = 0.05)
+    ),
+    column(2, offset = 0,
+           h5("Selective advantage of resistance"),
+           numericInput("s.RR1_A0", "locus1:", 0.5, min = 0.2, max = 1, step = 0.1),
+           numericInput("s.RR2_0B", "locus2:", 0.5, min = 0.2, max = 1, step = 0.1)
     )
-
+    
   )
 ))
