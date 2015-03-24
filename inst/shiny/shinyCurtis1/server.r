390bc1767e0c70d6edb3d4807d3477478fb373ec
AndySouth <southandy@gmail.com>
2015-03-24 12:51:59 +0000

    added exposure, corrected bug in dominance variable names (h.RS1_00 needed to be h.RS1_A0), added output of reproducible string

--- inst/shiny/shinyCurtis1/server.r
+++ inst/shiny/shinyCurtis1/server.r
@@ -11,16 +11,35 @@ shinyServer(function(input, output) {
   output$plot <- renderPlot({
     
 
-    cat("running resistSimple with these inputs:", input$P_1, input$P_2*input$P_1, input$h.RS1_00, input$h.RS2_00,"\n")
+    #cat("running resistSimple with these inputs:", input$P_1, input$P_2*input$P_1, input$h.RS1_00, input$h.RS2_00,"\n")
     
     resistSimple( P_1 = input$P_1, 
                   P_2 = input$P_2*input$P_1, #i need to put a limit of 1 on this
-                  h.RS1_00 = input$h.RS1_00, 
-                  h.RS2_00 = input$h.RS2_00,
+                  h.RS1_A0 = input$h.RS1_A0, 
+                  h.RS2_0B = input$h.RS2_0B,
+                  a.m_AB = input$a.m_AB,
+                  a.f_AB = input$a.m_AB, #set f to same as m
+                  a.m_00 = 1-input$a.m_AB, #set 00 to 1-AB
+                  a.f_00 = 1-input$a.m_AB, #set 00 to 1-AB
                   phi.SS1_A0 = input$phi.SS1_A0,
                   phi.SS2_0B = input$phi.SS2_0B,
                   s.RR1_A0 = input$s.RR1_A0,
                   s.RR2_0B = input$s.RR2_0B )
+    
+    #a hack to output the inputs
+    cat("resistSimple( P_1 =",input$P_1,",", 
+                  "P_2 =",input$P_2*input$P_1,",", 
+                  "h.RS1_A0 =",input$h.RS1_A0,",", 
+                  "h.RS2_0B =",input$h.RS2_0B,",",
+                  "a.m_AB =",input$a.m_AB,",",
+                  "a.f_AB =",input$a.m_AB,",", 
+                  "a.m_00 =",1-input$a.m_AB,",", 
+                  "a.f_00 =",1-input$a.m_AB,",", 
+                  "phi.SS1_A0 =",input$phi.SS1_A0,",",
+                  "phi.SS2_0B =",input$phi.SS2_0B,",",
+                  "s.RR1_A0 =",input$s.RR1_A0,",",
+                  "s.RR2_0B =",input$s.RR2_0B,")\n" )
+    
 
     
   })
--- inst/shiny/shinyCurtis1/ui.r
+++ inst/shiny/shinyCurtis1/ui.r
@@ -8,6 +8,8 @@ shinyUI(fluidPage(
   
   title = "Curtis insecticide resistance",
   
+  h3("Insecticide resistance explorer. Modify inputs below and the plot will respond"),
+  
   plotOutput('plot'),
   
   hr(),
@@ -20,11 +22,19 @@ shinyUI(fluidPage(
     ),
     column(2, offset = 0,
            h5("Dominance of resistance"),
-           numericInput("h.RS1_00", "locus1:", 0.2, min = 0, max = 1, step = 0.05),
-           numericInput("h.RS2_00", "locus2:", 0.2, min = 0, max = 1, step = 0.05)
+           numericInput("h.RS1_A0", "locus1:", 0.2, min = 0, max = 1, step = 0.05),
+           numericInput("h.RS2_0B", "locus2:", 0.2, min = 0, max = 1, step = 0.05)
     ),
     column(2, offset = 0,
-           h5("Fitness of susceptibles(SS) exposed"),
+           h5("Exposure to each insecticide"),
+           #in Curtis it assumes exposure to AB the same at 0.9 & same for M&F
+           numericInput("a.m_AB", "same for both insecticides in Curtis", 0.5, min = 0.1, max = 0.9, step = 0.1)
+           #later may want to allow setting diff exposure for each insecticide & gender
+           #numericInput("a.m_A", "insecticide1:", 0.5, min = 0.1, max = 0.9, step = 0.1),
+           #numericInput("a.m_B", "insecticide2:", 0.5, min = 0.1, max = 0.9, step = 0.1)
+    ),    
+    column(2, offset = 0,
+           h5("Fitness of susceptibles(SS) exposed to insecticide"),
            numericInput("phi.SS1_A0", "locus1:", 0.05, min = 0, max = 0.4, step = 0.05),
            numericInput("phi.SS2_0B", "locus2:", 0.05, min = 0, max = 0.4, step = 0.05)
     ),
