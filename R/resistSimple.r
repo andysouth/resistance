d5c48f11225b5f2785970d0115e6d6b3935c2ca5
AndySouth <southandy@gmail.com>
2015-03-24 10:46:14 +0000

    removed args, just anonymous that can be passed to setInputOneScenario

--- R/resistSimple.r
+++ R/resistSimple.r
@@ -2,22 +2,25 @@
 #' 
 #' May restructure later.  
 #' 
-#' @param calibration one of a limited set of integers effecting how scenarios are run 
-#' @param produce.plots whether to produce plots
-#' @param savePlots whether to save plots to hardcoded filenames
-#' @param save.fitvals whether to save fitness scores to csv 0=no, 1=yes 
+# cut these args out, defaults in setInputOneScenario()  
+# @param calibration one of a limited set of integers effecting how scenarios are run 
+# @param produce.plots whether to produce plots
+# @param savePlots whether to save plots to hardcoded filenames
+# @param save.fitvals whether to save fitness scores to csv 0=no, 1=yes 
+
 #' @param ... extra arguments passed to setInputOneScenario()
 #' 
 #' @return nothing yet
 #' @export
 #' 
-resistSimple <- function( calibration = 1012,
-                          produce.plots = TRUE,
-                          savePlots = FALSE,
-                          save.fitvals = 0,
-                          ...)
+resistSimple <- function(...)
 {
-  
+ 
+#cut these args out, defaults in setInputOneScenario()   
+#   calibration = 1012,
+#   produce.plots = TRUE,
+#   savePlots = FALSE,
+#   save.fitvals = 0,
   
   
   #calling new function to create input matrix
@@ -33,7 +36,7 @@ resistSimple <- function( calibration = 1012,
   # Run Model 
   # creates listOut$results, listOut$genotype & listOut$fitness
   # set produce.plots = FALSE so I can just have one plot later
-  listOut <- runModel( input, calibration, produce.plots = FALSE )
+  listOut <- runModel( input, produce.plots = FALSE )
   
   # Plot R and S allele frequencies over generations by M&F
   genplot <- plotallele.freq.andy( listOut$results[[1]] ) 
