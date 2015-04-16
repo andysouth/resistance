29964bb0dea96b10d5388d7e3d1eb6636d35e912
AndySouth <southandy@gmail.com>
2015-04-16 17:03:51 +0100

    starting to add classification tree code

--- R/sensiAn1.r
+++ R/sensiAn1.r
@@ -6,6 +6,7 @@
 #' 
 #' @examples
 #' sensiAn1( P_1=c(0,0.5) )
+#' sensiAn1( h.RS1_A0=c(0.1,1))
 #' @return todo
 #' @export
 #' 
@@ -16,18 +17,38 @@ sensiAn1 <- function( nScenarios = 10,
   input <- setInputSensiScenarios( nScenarios = nScenarios, ... )
   #input <- setInputSensiScenarios( nScenarios = nScenarios)  
 
-  listOut <- runModel(input)
+  #run the model using input which sets num scenarios
+  #suppress plotting to speed
+  listOut <- runModel(input, produce.plots = FALSE)
+  
+  #plot all scenario results on top of each other
   plotallele.freq.andy(listOut)
   
-  #extract timeToFifty from listOUt
+  #extract timetoFifty from listOUt
+  #todo replace with my own func
+  #note this is currently locus1
+  ttf <- timetoFifty(locus=1, max_gen=100, listOut$results, input)
   
+  #paste on to input
+  inputAndResults <- rbind(input,ttf)
   
   #transpose input, add results column extracted from listOut
-  
+  inputAndResults <- t(inputAndResults)
   
   #try a classification tree
+  #but may need to simplify outcome to 0/1 ??
+  #this from rpart vignette
+  #library(rpart)
+  #creates a factor for response var to improve labelling
+  #outcome <- factor(stagec$pgstat, levels = 0:1, labels = c("No", "Prog"))
+  #fit the model by adding the columns
+  #**me can I specify the predictors by names(match.call())[-1]**
+  #cfit <- rpart(progstat ~ age + eet + g2 + grade + gleason + ploidy, data = stagec, method = 'class')
+  #plot(cfit) #plots tree
+  #text(cfit) #labels tree
+  #*check how this relates to susanas method
   
   #not sure what to return yet
-  #invisible(listOut)
+  invisible(inputAndResults)
   
 }
