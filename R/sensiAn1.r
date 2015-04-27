1f5c90de1d29a6c555e23a9c0ad81fe6be0feb3a
AndySouth <southandy@gmail.com>
2015-04-27 14:01:29 +0100

    add experiments with rpart classification trees

--- R/sensiAn1.r
+++ R/sensiAn1.r
@@ -6,7 +6,10 @@
 #' 
 #' @examples
 #' sensiAn1( P_1=c(0,0.5) )
-#' sensiAn1( h.RS1_A0=c(0.1,1))
+#' inputAndResults <- sensiAn1( h.RS1_A0=c(0.1,1))
+#' colnames(inputAndResults)
+#' inputAndResults <- sensiAn1( h.RS1_A0=c(0.1,1), h.RS2_0B=c(0.1,1) )
+#' inputAndResults <- sensiAn1(500, h.RS1_A0=c(0.1,1), h.RS2_0B=c(0.1,1), s.RR1_A0=c(0.2,1), s.RR2_0B=c(0.2,1))
 #' @return todo
 #' @export
 #' 
@@ -34,21 +37,51 @@ sensiAn1 <- function( nScenarios = 10,
   
   #transpose input, add results column extracted from listOut
   inputAndResults <- t(inputAndResults)
+  #convert to a dataframe
+  inputAndResults <- data.frame(inputAndResults)
   
   #try a classification tree
   #but may need to simplify outcome to 0/1 ??
   #this from rpart vignette
   #library(rpart)
   #creates a factor for response var to improve labelling
-  #outcome <- factor(stagec$pgstat, levels = 0:1, labels = c("No", "Prog"))
+  #outcome <- factor(inputAndResults$Gens.to.0.5, levels = 0:1, labels = c("No", "Prog"))
+  #NAs are where 0.5 resistance not reached, convert them to 99 for now
+  inputAndResults$Gens.to.0.5[is.na(inputAndResults$Gens.to.0.5)] <- 999
+  inputAndResults$res <- cut(inputAndResults$Gens.to.0.5, breaks=c(0,50,9999),labels=c("<=50",">50"))
   #fit the model by adding the columns
   #**me can I specify the predictors by names(match.call())[-1]**
-  #cfit <- rpart(progstat ~ age + eet + g2 + grade + gleason + ploidy, data = stagec, method = 'class')
-  #plot(cfit) #plots tree
-  #text(cfit) #labels tree
-  #*check how this relates to susanas method
+  #cfit <- rpart(res ~ age + eet + g2 + grade + gleason + ploidy, data = inputAndResults, method = 'class')
   
-  #not sure what to return yet
+  #cfit <- rpart(res ~ h.RS1_A0 + h.RS2_0B, data = inputAndResults, method = 'class')
+  
+  tree <- rpart(res ~ h.RS1_A0 + h.RS2_0B + s.RR1_A0 + s.RR2_0B, data = inputAndResults, method = 'class')
+  
+  #including susanahs code to reduce branches using minsplit
+  tree <- rpart(res ~ h.RS1_A0 + h.RS2_0B + s.RR1_A0 + s.RR2_0B, data = inputAndResults, method = 'class', control=rpart.control(minsplit=50))
+  
+  #pruning tree
+  treePruned <- prune(tree, cp=tree$cptable[which.min(tree$cptable[,"xerror"]),"CP"])
+  plot(treePruned) #plots tree
+  text(treePruned) #labels tree
+    
+  
+  #can I use match.call to simplify setting the predictors to those passed by the user
+  #cfit <- rpart(res ~ names(match.call())[-1], data = inputAndResults, method = 'class')
+  
+  #can just separate out the data that is used in the model
+  #forCT <- inputAndResults[c('h.RS1_A0','h.RS2_0B','res')]
+  #cfit <- rpart(res ~ h.RS1_A0 + h.RS2_0B, data = forCT, method = 'class')
+
+  
+  plot(tree) #plots tree
+  text(tree) #labels tree
+
+  
+  #may want to return inputAndResults from this func
   invisible(inputAndResults)
+  #and then return tree or treePruned from another func
+  #invisible(tree)
+  #invisible(treePruned)
   
 }
