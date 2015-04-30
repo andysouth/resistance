767fc54de6aedb1a7723a34c73d41fc4ba412968
AndySouth <southandy@gmail.com>
2015-04-30 13:13:33 +0100

    add unit test for code refactoring, to test that my changes continue to give same results as Beths

---
+++ tests/testthat/testRefactoring.r
@@ -0,0 +1,20 @@
+library(resistance)
+
+context("Refactoring")
+
+test_that("refactoring named variables with arrays doesn't change results", {
+  
+  input <- setInputOneScenario()
+  
+  #expect refactored runModel2() to generate same results as runModel() for single default run
+  expect_identical( runModel2(input, produce.plots=FALSE),
+                    runModel(input, produce.plots=FALSE) )  
+  
+  
+  input <- setInputSensiScenarios(20, h.RS1_A0=c(0.1,1), h.RS2_0B=c(0.1,1), s.RR1_A0=c(0.2,1), s.RR2_0B=c(0.2,1))
+
+  
+  #expect refactored runModel2() to generate same results as runModel() for a range of scenarios
+  expect_identical( runModel2(input, produce.plots=FALSE),
+                    runModel(input, produce.plots=FALSE) )  
+})
