be51b0002ca2c4faf8c31da0418239e884c8bc00
AndySouth <southandy@gmail.com>
2015-05-01 17:44:35 +0100

    comment out examples for now while experimenting with rpart

--- R/sensiAn1.r
+++ R/sensiAn1.r
@@ -5,11 +5,11 @@
 #' @param ... parameter ranges to pass to \code{\link{setInputSensiScenarios}} in form c(min,max)
 #' 
 #' @examples
-#' sensiAn1( P_1=c(0,0.5) )
-#' inputAndResults <- sensiAn1( h.RS1_A0=c(0.1,1))
-#' colnames(inputAndResults)
-#' inputAndResults <- sensiAn1( h.RS1_A0=c(0.1,1), h.RS2_0B=c(0.1,1) )
-#' inputAndResults <- sensiAn1(500, h.RS1_A0=c(0.1,1), h.RS2_0B=c(0.1,1), s.RR1_A0=c(0.2,1), s.RR2_0B=c(0.2,1))
+#' #sensiAn1( P_1=c(0,0.5) )
+#' #inputAndResults <- sensiAn1( h.RS1_A0=c(0.1,1))
+#' #colnames(inputAndResults)
+#' #inputAndResults <- sensiAn1( h.RS1_A0=c(0.1,1), h.RS2_0B=c(0.1,1) )
+#' #inputAndResults <- sensiAn1(500, h.RS1_A0=c(0.1,1), h.RS2_0B=c(0.1,1), s.RR1_A0=c(0.2,1), s.RR2_0B=c(0.2,1))
 #' @return todo
 #' @export
 #' 
@@ -68,7 +68,7 @@ sensiAn1 <- function( nScenarios = 10,
 
   
   #using as.formula like this sorts uh-oh1
-  tree <- rpart(as.formula(paste("res ~",argString)), data = inputAndResults, method = 'class')  
+  tree <- rpart::rpart(as.formula(paste("res ~",argString)), data = inputAndResults, method = 'class')  
   
   #2 hardcoded args
   #tree <- rpart(res ~ h.RS1_A0 + h.RS2_0B, data = inputAndResults, method = 'class')
