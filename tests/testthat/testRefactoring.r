57a1c1ff2f6f85738c854421bd2083dc56639780
AndySouth <southandy@gmail.com>
2015-05-12 16:26:50 +0100

    set rand seed to make tests consistent

--- tests/testthat/testRefactoring.r
+++ tests/testthat/testRefactoring.r
@@ -12,11 +12,12 @@ test_that("refactoring named variables with arrays doesn't change results", {
   expect_equal( runModel2(input, produce.plots=FALSE),
                     runModel(input, produce.plots=FALSE) )  
   
-  
-  input <- setInputSensiScenarios(20, h.RS1_A0=c(0.1,1), h.RS2_0B=c(0.1,1), s.RR1_A0=c(0.2,1), s.RR2_0B=c(0.2,1))
+  #set random seed (may want to move option to setInputSensiScenarios() later )
+  set.seed(1)
+  input <- setInputSensiScenarios(30, h.RS1_A0=c(0.1,1), h.RS2_0B=c(0.1,1), s.RR1_A0=c(0.2,1), s.RR2_0B=c(0.2,1))
 
-  
   #expect refactored runModel2() to generate same results as runModel() for a range of scenarios
-  expect_equal( runModel2(input, produce.plots=FALSE),
-                    runModel(input, produce.plots=FALSE) )  
+  listOut2 <- runModel2(input, produce.plots=FALSE)
+  listOut <- runModel(input, produce.plots=FALSE)
+  expect_equal( listOut2, listOut )
 })
