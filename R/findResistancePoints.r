cfc37520d5ed47975ed9d0ff61a5d6440a834968
AndySouth <southandy@gmail.com>
2015-07-06 13:37:47 +0100

    allow to work on a single results matrix & refactor

--- R/findResistancePoints.r
+++ R/findResistancePoints.r
@@ -5,11 +5,18 @@
 #' Returns NA if freq does not reach 
 #' refactored from timetoFifty()
 
-#' @param listOut results & inputs from a set of scenario runs
+#' @param listOut results from a set of scenario runs (or a single results matrix)
 #' @param locus locus of interest (1,2, 'either' or 'both')
+#' @param criticalPoints the critical resistance thresholds that yopu want to assess
 # @param max_gen number of generations [ i don't think needed anymore ] 
 
-#' 
+#' @examples 
+#' input <- setInputSensiScenarios( nScenarios=3, h.RS1_A0=c(0,1) )
+#' listOut <- runModel2(input)
+#' # for all scenarios
+#' findResistancePoints(listOut, locus='both')
+#' # for selected scenarios
+#' findResistancePoints(listOut$results[[2]], locus='both')
 #' @return matrix with a column for each scenario, giving the generation number at which frequency of R is first greater than critical points
 #' @export
 
@@ -19,7 +26,8 @@ findResistancePoints <- function( listOut,
                                   criticalPoints = c(0.1,0.25,0.5)
                                   ){
   
-  numScenarios <- ncol(listOut$input)
+  if (class(listOut)=='matrix') numScenarios <- 1 
+  else numScenarios <- length(listOut$results) 
   
   #could first create matrix to receive the results, name rows including gen_cP for generations to reach criticalPoints
   resistGens <- matrix( nrow=length(criticalPoints), ncol=numScenarios, dimnames=list(paste0('gen_cP',criticalPoints) ) )
@@ -33,31 +41,36 @@ findResistancePoints <- function( listOut,
       criticalPoint <- criticalPoints[criticalPointNum]
       #cat("criticalPoint ", criticalPoint,"\n")
       
+      #getting results matrix out of the list
+      #this allows a single results matrix to be passed too
+      if (class(listOut)=='matrix') results <- listOut
+      else results <- listOut$results[[scenarioNum]]
+      
       resistances <- NULL
       if (locus==1)
         #mean of M&F
-        resistances <- rowMeans(cbind(listOut$results[[scenarioNum]][,'m.R1'],
-                                      listOut$results[[scenarioNum]][,'f.R1'] ))
+        resistances <- rowMeans(cbind(results[,'m.R1'],
+                                      results[,'f.R1'] ))
       else if (locus==2)
         #mean of M&F
-        resistances <- rowMeans(cbind(listOut$results[[scenarioNum]][,'m.R2'],
-                                      listOut$results[[scenarioNum]][,'f.R2'] ))
+        resistances <- rowMeans(cbind(results[,'m.R2'],
+                                      results[,'f.R2'] ))
       else if (locus=='either')
         #calculate the parallel maximum of the 2 means
         #to find the 1st locus that reaches threshold
         resistances <- pmax(
-                        rowMeans(cbind(listOut$results[[scenarioNum]][,'m.R1'],
-                                       listOut$results[[scenarioNum]][,'f.R1'] )),
-                        rowMeans(cbind(listOut$results[[scenarioNum]][,'m.R2'],
-                                       listOut$results[[scenarioNum]][,'f.R2'] )) )
+                        rowMeans(cbind(results[,'m.R1'],
+                                       results[,'f.R1'] )),
+                        rowMeans(cbind(results[,'m.R2'],
+                                       results[,'f.R2'] )) )
       else if (locus=='both')      
         #calculate the parallel minimum of the 2 means
         #to find the 2nd locus that reaches threshold
         resistances <- pmin(
-          rowMeans(cbind(listOut$results[[scenarioNum]][,'m.R1'],
-                         listOut$results[[scenarioNum]][,'f.R1'] )),
-          rowMeans(cbind(listOut$results[[scenarioNum]][,'m.R2'],
-                         listOut$results[[scenarioNum]][,'f.R2'] )) )
+          rowMeans(cbind(results[,'m.R1'],
+                         results[,'f.R1'] )),
+          rowMeans(cbind(results[,'m.R2'],
+                         results[,'f.R2'] )) )
       else
         stop("locus is not one of 1,2,'either','both':", locus)
              
