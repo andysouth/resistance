93058b6512130c84ccf5450ceebe7e4d31284643
AndySouth <southandy@gmail.com>
2015-07-08 22:29:40 +0100

    add 2 plots, action button and new text

--- inst/shiny/shinyFig2Curtis/server.r
+++ inst/shiny/shinyFig2Curtis/server.r
@@ -1,5 +1,5 @@
-#resistance/shiny/shinyCurtis1/server.r
-#andy south 22/3/15
+#resistance/shiny/shinyFig2Curtis/server.r
+#andy south 8/7/15
 
 library(shiny)
 library(resistance)
@@ -7,42 +7,49 @@ library(resistance)
 
 shinyServer(function(input, output) {
   
+  output$plotDefault <- renderPlot({
+    
+    #isolate reactivity of other objects
+    isolate({
+      runcurtis_f2()
+    }) #end isolate 
+  })    
+    
   
   output$plot <- renderPlot({
     
-
-    #cat("running resistSimple with these inputs:", input$P_1, input$P_2*input$P_1, input$h.RS1_00, input$h.RS2_00,"\n")
-    
-    resistSimple( P_1 = input$P_1, 
-                  P_2 = input$P_2,
-                  #P_2 = input$P_2*input$P_1, #if doing 2 as a proportion of 1, i need to put a limit of 1 on this
-                  h.RS1_A0 = input$h.RS1_A0, 
-                  h.RS2_0B = input$h.RS2_0B,
-                  a.m_AB = input$a.m_AB,
-                  a.f_AB = input$a.m_AB, #set f to same as m
-                  a.m_00 = 1-input$a.m_AB, #set 00 to 1-AB
-                  a.f_00 = 1-input$a.m_AB, #set 00 to 1-AB
-                  phi.SS1_A0 = input$phi.SS1_A0,
-                  phi.SS2_0B = input$phi.SS2_0B,
-                  s.RR1_A0 = input$s.RR1_A0,
-                  s.RR2_0B = input$s.RR2_0B )
-    
-    #a hack to output the inputs
-    cat("resistSimple( P_1 =",input$P_1,",", 
-                  "P_2 =",input$P_2,",", 
-                  "h.RS1_A0 =",input$h.RS1_A0,",", 
-                  "h.RS2_0B =",input$h.RS2_0B,",",
-                  "a.m_AB =",input$a.m_AB,",",
-                  "a.f_AB =",input$a.m_AB,",", 
-                  "a.m_00 =",1-input$a.m_AB,",", 
-                  "a.f_00 =",1-input$a.m_AB,",", 
-                  "phi.SS1_A0 =",input$phi.SS1_A0,",",
-                  "phi.SS2_0B =",input$phi.SS2_0B,",",
-                  "s.RR1_A0 =",input$s.RR1_A0,",",
-                  "s.RR2_0B =",input$s.RR2_0B,")\n" )
-    
-
-    
+    #add dependency on the button
+    if ( input$aButtonRun > 0 ) 
+    {
+      #isolate reactivity of other objects
+      isolate({
+        
+        #cat("running resistSimple with these inputs:", input$P_1, input$P_2*input$P_1, input$h.RS1_00, input$h.RS2_00,"\n")
+        
+        runcurtis_f2( P_1 = input$P_1, 
+                      P_2 = input$P_2,
+                      #P_2 = input$P_2*input$P_1, #if doing 2 as a proportion of 1, i need to put a limit of 1 on this
+                      h.RS1_A0 = input$h.RS1_A0, 
+                      h.RS2_0B = input$h.RS2_0B,
+                      exposure = input$exposure,
+                      phi.SS1_A0 = input$phi.SS1_A0,
+                      phi.SS2_0B = input$phi.SS2_0B,
+                      s.RR1_A0 = input$s.RR1_A0,
+                      s.RR2_0B = input$s.RR2_0B )
+        
+        #a hack to output the inputs
+        cat("runcurtis_f2( P_1 =",input$P_1,",", 
+                      "P_2 =",input$P_2,",", 
+                      "h.RS1_A0 =",input$h.RS1_A0,",", 
+                      "h.RS2_0B =",input$h.RS2_0B,",",
+                      "exposure =",input$exposure,",",
+                      "phi.SS1_A0 =",input$phi.SS1_A0,",",
+                      "phi.SS2_0B =",input$phi.SS2_0B,",",
+                      "s.RR1_A0 =",input$s.RR1_A0,",",
+                      "s.RR2_0B =",input$s.RR2_0B,")\n" )
+        
+      }) #end isolate  
+    } #end if ( input$aButtonRun > 0 ) 
   })
   
 })
--- inst/shiny/shinyFig2Curtis/ui.r
+++ inst/shiny/shinyFig2Curtis/ui.r
@@ -1,5 +1,5 @@
-#resistance/shiny/shinyCurtis1/ui.r
-#andy south 22/3/15
+#resistance/shiny/shinyFig2Curtis/ui.r
+#andy south 7/8/15
 
 library(shiny)
 
@@ -8,42 +8,50 @@ shinyUI(fluidPage(
   
   title = "Curtis insecticide resistance",
   
-  h3("Insecticide resistance explorer. Modify inputs below and the plot will respond."),
+  h4("Plots will appear below in a few seconds."),  
+  h4("Left plot is Fig.2 from Curtis. Right plot is set by inputs below (initially set to recreate Curtis)."),
+  h4("Tweak inputs and press lower button to change right plot."),  
   
-  plotOutput('plot'),
+  fluidRow(
+    column(6, plotOutput('plotDefault')),
+    column(6, plotOutput('plot'))
+  ), #end fluid row
   
   hr(),
   
   fluidRow(
     column(2,
            h5("Starting frequency of resistance"),
-           numericInput("P_1", "locus1:", 0.05, min = 0.01, max = 0.1, step = 0.01),
-           numericInput("P_2", "locus2:", 0.05, min = 0.01, max = 0.1, step = 0.01)
+           numericInput("P_1", "locus1:", 0.01, min = 0.0001, max = 0.1, step = 0.001),
+           numericInput("P_2", "locus2:", 0.01, min = 0.0001, max = 0.1, step = 0.001)
            #numericInput("P_2", "locus2 - proportion of locus1:", 0.1, min = 0.01, max = 100, step = 0.01)
     ),
     column(2, offset = 0,
            h5("Dominance of resistance"),
-           numericInput("h.RS1_A0", "locus1:", 0.2, min = 0, max = 1, step = 0.05),
-           numericInput("h.RS2_0B", "locus2:", 0.2, min = 0, max = 1, step = 0.05)
+           numericInput("h.RS1_A0", "locus1:", 0.17, min = 0, max = 1, step = 0.01),
+           numericInput("h.RS2_0B", "locus2:", 0.0016, min = 0, max = 1, step = 0.01)
     ),
     column(2, offset = 0,
            h5("Exposure to each insecticide"),
            #in Curtis it assumes exposure to AB the same at 0.9 & same for M&F
-           numericInput("a.m_AB", "same for both insecticides in Curtis", 0.5, min = 0.1, max = 0.9, step = 0.1)
+           numericInput("exposure", "same for both insecticides in Curtis", 0.9, min = 0.1, max = 1, step = 0.05)
            #later may want to allow setting diff exposure for each insecticide & gender
            #numericInput("a.m_A", "insecticide1:", 0.5, min = 0.1, max = 0.9, step = 0.1),
            #numericInput("a.m_B", "insecticide2:", 0.5, min = 0.1, max = 0.9, step = 0.1)
     ),    
     column(2, offset = 0,
            h5("Fitness of susceptibles(SS) exposed to insecticide"),
-           numericInput("phi.SS1_A0", "locus1:", 0.05, min = 0, max = 0.4, step = 0.05),
-           numericInput("phi.SS2_0B", "locus2:", 0.05, min = 0, max = 0.4, step = 0.05)
+           numericInput("phi.SS1_A0", "locus1:", 0.73, min = 0, max = 1, step = 0.05),
+           numericInput("phi.SS2_0B", "locus2:", 1, min = 0, max = 1, step = 0.05)
     ),
     column(2, offset = 0,
            h5("Selective advantage of resistance"),
-           numericInput("s.RR1_A0", "locus1:", 0.5, min = 0.2, max = 1, step = 0.1),
-           numericInput("s.RR2_0B", "locus2:", 0.5, min = 0.2, max = 1, step = 0.1)
+           numericInput("s.RR1_A0", "locus1:", 0.23, min = 0, max = 1, step = 0.05),
+           numericInput("s.RR2_0B", "locus2:", 0.43, min = 0, max = 1, step = 0.05)
     )
-    
-  )
+  ), #end fluid row
+  fluidRow(
+    column(4, hr()),
+    column(2, actionButton('aButtonRun', 'Run Model (takes a few seconds'))
+  ) #end fluid row
 ))
