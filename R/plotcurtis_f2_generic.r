cc6c892a79a576e9163c7aed818c8d8bbbe3570e
AndySouth <southandy@gmail.com>
2015-07-22 16:05:33 +0100

    added option for the combined mixture strategy but set default to FALSE because it makes the plot confusing

--- R/plotcurtis_f2_generic.r
+++ R/plotcurtis_f2_generic.r
@@ -12,13 +12,21 @@
 #' @param r2col column number containing frequency of allele at R2
 #' @param main title for the plot
 #' @param criticalPoints critical resistance points to be displayed in the plot
+#' @param addCombinedStrategy whether to add a line for the combined mixture/individual strategy, makes plot confusing so set to FALSE by default
+#' 
+#' @examples
+#' inputAndResultsMix <- sensiAnPaperPart( 2, insecticideUsed = 'mixture' )
+#' inputAndResultsI1 <- sensiAnPaperPart( 2, insecticideUsed = 'insecticide1' )
+#' inputAndResultsI2 <- sensiAnPaperPart( 2, insecticideUsed = 'insecticide2' )
+#' plotcurtis_f2_generic( listOutMix$results[[1]], listOutI2$results[[1]], listOutI1$results[[1]] )
 #' 
 #' @return list of plot components
 #' @export
 
 plotcurtis_f2_generic <- function( combmat, bmat, amat, gencol=1, r1col=2, r2col=3,
                                    main="Rise of resistance; sequential insecticides v mixtures",
-                                   criticalPoints = c(0.1,0.25,0.5) 
+                                   criticalPoints = c(0.1,0.25,0.5),
+                                   addCombinedStrategy = FALSE
                                    ){
   
   #find generations to reach max resistance points
@@ -68,6 +76,8 @@ plotcurtis_f2_generic <- function( combmat, bmat, amat, gencol=1, r1col=2, r2col
   axis( side=2, at=logylabs, labels=ylabsnames, tick=T )
   
   ## Single insecticide scenarios
+  #andy remember that in this from Beth hch is insecticide1 & ddt is insecticide2
+  
   # Only plots up to generation where 0.5 is first reached/exceeded, so this generation is found
   hch_cutoff <- min(which((amat[,r1col])>max(criticalPoints)))
   ddt_cutoff <- min(which((bmat[,r2col])>max(criticalPoints)))
@@ -81,10 +91,49 @@ plotcurtis_f2_generic <- function( combmat, bmat, amat, gencol=1, r1col=2, r2col
   hch_gens <- c(1:hch_cutoff)
   ddt_gens <- c((hch_cutoff):(hch_cutoff+(ddt_cutoff-1)))
   
-  
   # add these as lines
   lines( hch_gens, hch, col="red", lty=2 )
   lines( ddt_gens, ddt, col="blue", lty=2 )
+
+  #andy added
+  if( addCombinedStrategy )
+  {
+    cutoff_i1_mix <- min(which((combmat[,r1col])>max(criticalPoints)))
+    cutoff_i2_mix <- min(which((combmat[,r2col])>max(criticalPoints)))      
+    
+    #trying to add a dotted line for the combined strategy
+    #it should start at the point that the resistance to the first insecticide is reached
+    #and go parallel to the other individual insecticide after
+    #but remember that resistance may be reached for insecticide2 first
+    if ( cutoff_i1_mix < cutoff_i2_mix )
+    {
+      #finding which generations
+      #initially just use maxGens for end
+      #gens <- c( cutoff_i1_mix : (maxGens-cutoff_i1_mix) )
+      gens <- c( cutoff_i1_mix : (cutoff_i1_mix+length(ddt)-1 ) )
+      #this is tricky
+      #have to log the starting point
+      #find resistance level for i2 at the point the cutoff is reached for i1
+      startR <- log10( 100 * combmat[cutoff_i1_mix,r2col] )      
+      #add the individual use line, with the difference between the indiv start and the new start
+      lines( gens, ddt-ddt[1]+startR, col="blue", lty=3 )
+      
+    } else #if cutoff_i2_mix <= cutoff_i1_mix
+    {
+      
+      gens <- c( cutoff_i2_mix : (cutoff_i2_mix+length(hch)-1 ) )
+
+      #find resistance level for i1 at the point the cutoff is reached for i2            
+      startR <- log10( 100 * combmat[cutoff_i2_mix,r1col] )      
+      #add the individual use line, with the difference between the indiv start and the new start     
+      lines( gens, hch-hch[1]+startR, col="red", lty=3 )   
+      
+    }
+    
+
+  }  
+  
+  
   
   #when the switch is made, was hardcoded by Beth
   #abline( v = 31, col="black" )
@@ -97,9 +146,19 @@ plotcurtis_f2_generic <- function( combmat, bmat, amat, gencol=1, r1col=2, r2col
 #   yval <- log10( 2 )
 #   legend( 105, yval, legend=c("Sequential", "Combination","R at Locus 1 (DDT)", "R at Locus 2(HCH)"), 
 #           col=c("black","black","red", "darkblue"), lty=c(2,1,1,1), bty="n", cex=0.7 )
-    legend( 'bottomright', legend=c("Sequential", "Mixture", "Insecticide 1", "Insecticide 2"), 
-            col=c("black","black","red", "darkblue"), lty=c(2,1,1,1), bty="n", cex=0.7 )  
-			
+  if( addCombinedStrategy )
+  {
+    #added a dotted line for combined
+    legend( 'bottomright', legend=c("sequential", "mixture", "combined", "insecticide 1", "insecticide 2"), 
+            col=c("black","black","black","red", "blue"), lty=c(2,1,3,1,1), bty="n", cex=0.7 )     
+  } else
+  {
+    legend( 'bottomright', legend=c("sequential", "mixture", "insecticide 1", "insecticide 2"), 
+            col=c("black","black","red", "blue"), lty=c(2,1,1,1), bty="n", cex=0.7 )    
+  }
+ 
+
+    			
   
   box()
   
