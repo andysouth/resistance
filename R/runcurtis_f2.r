9c54bc67c87913fade22c1f31f0feafdee07464c
AndySouth <southandy@gmail.com>
2016-03-22 17:47:41 +0000

    add doc for args plus additional args

--- R/runcurtis_f2.r
+++ R/runcurtis_f2.r
@@ -2,7 +2,37 @@
 #' 
 #' Recreates figure 2 of Curtis(1985), allows inputs to be tweaked
 #' 
-#' 
+#' @param max_gen number of generations
+#' @param P_1 locus 1 frequency of resistance allele
+#' @param P_2 locus 2 frequency of resistance allele
+#' @param recomb_rate recombination rate
+#' @param exposure the single exposure param used to generate the array
+#' @param phi.SS1_a0 Baseline fitness of SS1 in a0
+#' @param phi.SS1_A0 Baseline fitness of SS1 in A0
+#' @param phi.SS2_0b Baseline fitness of SS2 in 0b
+#' @param phi.SS2_0B Baseline fitness of SS2 in 0B
+#' @param W.SS1_00 Fitness of SS1 in 00 (no insecticide)
+#' @param W.SS2_00 Fitness of SS2 in 00 (no insecticide)
+#' @param h.RS1_00 Dominance coefficient locus1 in 00
+#' @param h.RS1_a0 Dominance coefficient locus1 in a0
+#' @param h.RS1_A0 Dominance coefficient locus1 in A0
+#' @param h.RS2_00 Dominance coefficient locus2 in 00
+#' @param h.RS2_0b Dominance coefficient locus2 in 0b
+#' @param h.RS2_0B Dominance coefficient locus2 in 0B
+#' @param s.RR1_a0 Selection coefficient locus1 in a
+#' @param s.RR1_A0 Selection coefficient locus1 in A
+#' @param s.RR2_0b Selection coefficient locus2 in b
+#' @param s.RR2_0B Selection coefficient locus2 in B
+#' @param z.RR1_00 fitness cost of resistance allele 1 in insecticide free environment
+#' @param z.RR2_00 fitness cost of resistance allele 2 in insecticide free environment
+#' @param sexLinked whether resistance is sex linked, default=0(FALSE)
+#' @param maleExposureProp male exposure as a propoertion of female, default 1 for same, likely <1  
+#' @param correctMixDeployProp proportion of correct deployment of mixtures, 
+#'    if <1 other portion divided between single insecticides
+#' @param addCombinedStrategy whether to add a line for the combined mixture/individual strategy, makes plot confusing so set to FALSE by default
+#' @param addStrategyLabels add labels to plot for where strategies cross 0.5 line TRUE gives seq,mix1,mix2,mix3
+#' @param strategyLabels = c("seq","mix1","adapt","mix2")
+#'  
 #' @return list of plot components
 #' @export
 
@@ -30,9 +60,11 @@ runcurtis_f2 <- function( max_gen = 500,
                           z.RR1_00	=	0	,
                           z.RR2_00	=	0	,
                           sexLinked = 0 ,
-                          addCombinedStrategy = TRUE,
                           maleExposureProp = 1,
-                          correctMixDeployProp = 1  )
+                          correctMixDeployProp = 1,
+                          addCombinedStrategy = TRUE,
+                          addStrategyLabels = TRUE,
+                          strategyLabels = c("seq","mix1","adapt","mix2") )
 {
   
   #fill exposure array I1
@@ -137,11 +169,15 @@ runcurtis_f2 <- function( max_gen = 500,
   #run model
   listOut <- runModel2(input=input, produce.plots = FALSE)
   
-  #this is a modifdeied version of Beths function
+  #this is a modified version of Beths function
   #allowing curtis fig2 to be applied to any scenario
   #plotcurtis_f2_generic( listOutMix$results[[1]], listOutI2$results[[1]], listOutI1$results[[1]] )
   #be careful of order
   #mix,2,1
-  plotcurtis_f2_generic( listOut$results[[3]], listOut$results[[2]], listOut$results[[1]], addCombinedStrategy = addCombinedStrategy )  
+  plotcurtis_f2_generic( listOut$results[[3]], listOut$results[[2]], listOut$results[[1]], 
+                         addCombinedStrategy = addCombinedStrategy,
+                         addStrategyLabels = addStrategyLabels,
+                         strategyLabels = strategyLabels
+                         )  
   
 }
