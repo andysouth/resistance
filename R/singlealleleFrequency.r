429cc8a91d2d493e1b9d669693e4b1ae75658fa1
AndySouth <southandy@gmail.com>
2016-06-01 12:02:39 +0100

    improve var names

--- R/singlealleleFrequency.r
+++ R/singlealleleFrequency.r
@@ -14,9 +14,10 @@
 
 singlealleleFrequency <- function(locus, max_gen, results.list, input){
   
-  k <- ncol(input)  	#number of scenarios is column number of input matrix
+  num_scenarios <- ncol(input)  	#number of scenarios is column number of input matrix
   
-  freq <- matrix( ncol=k, nrow=max_gen )	#matrix with column for each scenario of interest and row for freq of R at each generation
+  freq <- matrix( ncol=num_scenarios, nrow=max_gen )	#matrix with column for each scenario of interest and row for freq of R at each generation
+  
   rownames(freq) <- c(1:max_gen)
   colnames(freq) <- colnames(input)
   
@@ -27,8 +28,9 @@ singlealleleFrequency <- function(locus, max_gen, results.list, input){
     col <- 3			#if locus 2 is selected, the results are extracted from column 3 of the results matrix
   }
   
-  for (i in 1:k){
-    mat <- results.list[[i]]		# for each scenario, extracts matrix of results from results list
+  for (scenario_num in 1:num_scenarios){
+    
+    mat <- results.list[[scenario_num]]		# for each scenario, extracts matrix of results from results list
     res <- mat[,col]				# extracts column of results for desired locus from the matrix
     
     if( length(res) != max_gen ){		# if the length of the vector produced is not equal to the value set for max_gen
@@ -37,7 +39,7 @@ singlealleleFrequency <- function(locus, max_gen, results.list, input){
       res <- c(res,add)				# concatenates this vector to make the vector the same length as the maximum length generated
     }
     
-    freq[,i] <- res					# for that scenario run, prints to corresponding column in the allele frequency matrix
+    freq[,scenario_num] <- res					# for that scenario run, prints to corresponding column in the allele frequency matrix
   }
   
   return( freq )
