6cdc8ec1c69c38d8c3599bedfb70f9863de8e58a
AndySouth <southandy@gmail.com>
2016-06-03 16:10:47 +0100

    sorted responsive display on mobile

--- inst/shiny/resistmob1/ui.r
+++ inst/shiny/resistmob1/ui.r
@@ -5,21 +5,39 @@
 library(shiny)
 
 
-shinyUI(fluidPage(
-#shinyUI(fixedPage(
+shinyUI(fluidPage( theme = "bootstrap_simplex.css",
+#shinyUI(fixedPage( theme = "bootstrap_simplex.css",
  
   #can add CSS controls in here
   #http://shiny.rstudio.com/articles/css.html
-  #this seems to make no difference yet
+  #http://www.w3schools.com/css/css_rwd_mediaqueries.asp
   tags$head(
     tags$style(HTML("
-                    img {
-                    border: 1;
-                    max-width: 100%;
+
+                    [class*='col-'] {
+                        padding: 5px;
+                        border: 1px;
+                        position: relative;
+                        min-height: 1px;
                     }
-                    element.style {
-                    width: 33.33%;
+
+                    .container {
+                      margin-right: 0;
+                      margin-left: 0;
+                      float: left;
                     }
+                    .col-sm-1 {width: 8.33%; float: left;}
+                    .col-sm-2 {width: 16.66%; float: left;}
+                    .col-sm-3 {width: 25%; float: left;}
+                    .col-sm-4 {width: 33.33%; float: left;}
+                    .col-sm-5 {width: 41.66%; float: left;}
+                    .col-sm-6 {width: 50%;  float: left; padding: 0px;} !to make more space for plots
+                    .col-sm-7 {width: 58.33%; float: left;}
+                    .col-sm-8 {width: 66.66%; float: left;}
+                    .col-sm-9 {width: 75%; float: left;}
+                    .col-sm-10 {width: 83.33%; float: left;}
+                    .col-sm-11 {width: 91.66%; float: left;}
+                    .col-sm-12 {width: 100%; float: left;}
                     
                     "))
     ),  
@@ -37,11 +55,11 @@ shinyUI(fluidPage(
   ), #end fixed row
   
   fluidRow(
-    #column(4, h5("takes a few secounds :")),
+    #column(4, h5("takes a few seconds :")),
     column(2, p()),
     column(2, actionButton('aButtonRunA', 'Run Scenario A')),
     column(1, p()),   
-    column(3, h6("(takes a few secounds)")),
+    column(3, h6("(takes a few seconds)")),
     column(2, actionButton('aButtonRunB', 'Run Scenario B')),
     column(2, p())
   ), #end fixed row
@@ -60,7 +78,7 @@ shinyUI(fluidPage(
     ),
     column(2,
            #h5("Starting frequency of resistance"),
-           h5("Start Frequency"),
+           h5("Start Freq."),
            #numericInput("P_1", "locus1: 0.01", 0.01, min = 0.0001, max = 0.1, step = 0.001),
            #numericInput("P_2", "locus2: 0.01", 0.01, min = 0.0001, max = 0.1, step = 0.001)
            sliderInput("frequency_A1", NULL, val=0.01, min = 0.0001, max = 0.1, step = 0.001, ticks=FALSE),
@@ -71,7 +89,7 @@ shinyUI(fluidPage(
            
     ),    
     column(2, offset = 0,
-           h5("Dominance of resistance"),
+           h5("Dominance"),
            #numericInput("h.RS1_A0", "locus1: 0.17", 0.17, min = 0, max = 1, step = 0.01),
            #numericInput("h.RS2_0B", "locus2: 0.0016", 0.0016, min = 0, max = 1, step = 0.01)
            sliderInput("dominance_A1", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
@@ -81,7 +99,7 @@ shinyUI(fluidPage(
            sliderInput("dominance_B2", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE)
     ),
     column(2, offset = 0,
-           h5("Exposure to insecticide"),
+           h5("Exposure"),
            #in Curtis it assumes exposure to AB the same at 0.9 & same for M&F
            #numericInput("exposure", "same for both insecticides in Curtis 0.9", 0.9, min = 0.1, max = 1, step = 0.05)
            #sliderInput("exposure", NULL, val=0.2, min = 0, max = 1, step = 0.1, ticks=FALSE),
@@ -94,7 +112,7 @@ shinyUI(fluidPage(
     ),    
     column(2, offset = 0,
            #h5("Selection against susceptibles(SS) exposed to insecticide"),
-           h5("Effectiveness of insecticides"),
+           h5("Effectiveness"),
            #numericInput("phi.SS1_A0", "locus1: 0.73", 0.73, min = 0, max = 1, step = 0.05),
            #numericInput("phi.SS2_0B", "locus2: 1", 1, min = 0, max = 1, step = 0.05)
            sliderInput("effectiveness_A1", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
@@ -105,8 +123,8 @@ shinyUI(fluidPage(
            
     ),
     column(2, offset = 0,
-           #h5("Selective advantage of resistance"),
-           h5("Advantage of resistance"),
+           #h5("Advantage of resistance"),
+           h5("Advantage"),
            #numericInput("s.RR1_A0", "locus1: 0.23", 0.23, min = 0, max = 1, step = 0.05),
            #numericInput("s.RR2_0B", "locus2: 0.43", 0.43, min = 0, max = 1, step = 0.05),
            
