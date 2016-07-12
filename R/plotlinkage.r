4b6c8b8d383e36c0d5367f46778ea88151882439
AndySouth <southandy@gmail.com>
2016-07-12 11:02:38 +0100

    add flaxibility to plot m&f and start dprime

--- R/plotlinkage.r
+++ R/plotlinkage.r
@@ -1,6 +1,13 @@
 #' plot Linkage Disequilibrium over generations
 #' 
 #' @param mat results matrix
+#' @param plot_d
+#' @param plot_dprime
+#' @param main plot title
+#' @param xlab xlab
+#' @param ylab ylab
+#' @param addLegend whether to add a legend inside plot
+#' @param legpos legend position
 #' 
 #' @examples 
 #' #single scenario
@@ -10,12 +17,58 @@
 #' @return list of plot components
 #' @export
 
-plotlinkage <- function ( mat ){	
+plotlinkage <- function ( mat, 
+                          plot_d = TRUE,
+                          plot_dprime = TRUE,
+                          main="",
+                          xlab="Generation",
+                          ylab="Linkage Disequilibrium",
+                          addLegend=TRUE,
+                          legpos="topright"){	
   
-  par(pty="s") 		
-  LD <- mat[,4]	 ## set LD column as vector (m LD = f LD)	
-  gens <- mat[,1]									
-  plot( gens, LD, type="l", axes=T,						## Blank square 1,1 plot
-        xlim=c(1,(max(gens))), ylim=c(min(LD),max(LD)),
-        xlab="Generation", ylab="Linkage Disequilibrium (D)", main="Linkage disequilibrium over generations.")	
+  par(pty="s") #makes plot square	
+  
+  ld_male <- mat[,4]	 # 4 = male
+  ld_female <- mat[,7]   # 7 = female
+  ld_prime_m <- mat[,10]  #10 = male prime
+  #ld_prime_f <- mat[,12]  #10 = female prime
+  
+  ld_concat <- NULL
+  
+  if (plot_d) ld_concat <- c(ld_male, ld_female)
+    
+  if (plot_dprime) ld_concat <- c(ld_concat, ld_prime_m)
+  
+  gens <- mat[,1]	
+  
+  plot( gens, ld_male, type="n", axes=T,						## Blank square plot
+        xlim=c(1,(max(gens))), ylim=c(min(ld_concat),max(ld_concat)),
+        xlab=xlab, ylab=ylab, main=main ) #"Linkage disequilibrium over generations.")	
+  
+  if (plot_d)
+  {
+    lines( gens, ld_female, col=adjustcolor("red", alpha.f = 0.5), lty=13 )
+    lines( gens, ld_male, col=adjustcolor("blue", alpha.f = 0.5), lty=14 )    
+  }
+
+  if (plot_dprime)
+  {  
+    #lines( gens, ld_prime_f, col=adjustcolor("red", alpha.f = 0.5), lty=15 )
+    lines( gens, ld_prime_m, col=adjustcolor("blue", alpha.f = 0.5), lty=16 )
+  }
+  
+  if (plot_d & plot_dprime){
+    legend( legpos, legend=c("female","male","prime_m"), 
+            col=c("red", "blue", "blue"), lty=c(13,14,16), bty="n", cex=0.7 )
+    
+  } else if (plot_d)
+  {
+    legend( legpos, legend=c("female","male"), 
+            col=c("red", "blue"), lty=c(13,14), bty="n", cex=0.7 )    
+  } else if (plot_dprime)
+  {
+    legend( legpos, legend=c("prime_m"), 
+            col=c("blue"), lty=c(16), bty="n", cex=0.7 )   
+  }
+  
 }	
