03819188e6483b2756134a9269fbb1803c81d334
AndySouth <southandy@gmail.com>
2016-09-27 18:00:46 +0100

    move code into fitnessOutput()

---
+++ R/fitnessOutput.r
@@ -0,0 +1,52 @@
+#' output fitness by niche
+#' 
+#' @param Wniche array of niche fitnesses
+#' 
+#' @examples 
+#' #fitnessOutput()
+
+#' @return fitness values in a matrix
+#' @export
+
+fitnessOutput <- function ( Wniche = NULL )
+{
+  
+  ## output fitnesses by niche & genotype
+  
+  fbn <- matrix( ncol=9, nrow=9 )
+  
+  colnames(fbn) <- c("-,-", "a,-", "A,-", "-,b", "-,B", "a,b", "A,B", "A,b", "a,B")
+  #default order is different : 00 a0 A0 0b ab Ab 0B aB AB
+  #potential problems in ordering of rows & cols avoided by filling by name below
+  
+  rownames(fbn) <- c("SS1SS2", "SS1RS2", "SS1RR2",
+                     "RS1SS2", "RS1RS2", "RS1RR2",
+                     "RR1SS2", "RR1RS2", "RR1RR2" )
+  
+  for( locus1 in dimnames(Wniche)$locus1)
+  {
+    for( locus2 in dimnames(Wniche)$locus2)
+    {
+      #this is a good way of doing but the columns end in a different order
+      #which wouldn't be a problem except that initially I'm trying
+      #to keep results identical to Beths
+      #fbn[paste0(locus1,locus2),] <- Wniche[locus1,locus2,,]
+      #so instead go through each niche
+      for( niche1 in dimnames(Wniche)$niche1)
+      {
+        for( niche2 in dimnames(Wniche)$niche2)
+        {
+          #get columnName by converting 0 to - and inserting commas
+          columnName <- paste0(niche1,",",niche2)
+          columnName <- gsub('0','-',columnName)
+          #get rowname by pasting
+          rowName <- paste0(locus1,locus2)
+          
+          fbn[rowName,columnName] <- Wniche[locus1,locus2,niche1,niche2]
+        }
+      }
+    }
+  }
+  
+  return(fbn)  
+}
--- R/runModel2.r
+++ R/runModel2.r
@@ -186,7 +186,7 @@ runModel2 <- function(input,
     #extra check in case NA value gets in
     if ( is.na(sexLinked) | !is.logical(sexLinked) ) sexLinked <- FALSE
     
-    #### end of reading inputs into parameters
+    ## end of reading inputs into parameters
     
     # Set up matrices to output results to
     # matrix:results - for overall freq of R and S allele per locus per sex, LD and overall allele freq (i.e. 1)
@@ -251,8 +251,6 @@ runModel2 <- function(input,
            
     ##################
     ## generation loop
-    
-    
     for (k in 1:max_gen){
       
       # In calibration 1011, selection relaxed for a set time
@@ -299,8 +297,7 @@ runModel2 <- function(input,
       ## Random Mating
       # males & females will only be different if sexLinked=TRUE
       
-      # males
-      # initially by calculating 'expanded' genotypes
+      # males (initially by calculating 'expanded' genotypes)
       fGenotypeExpanded <- randomMating(G, sexLinked=sexLinked, isMale=TRUE)
       f['m',] <- genotypesLong2Short(fGenotypeExpanded)
       
@@ -320,63 +317,20 @@ runModel2 <- function(input,
         f['f', ] <- genotype.freq[]
       }
       
-    }	####### end of generation loop 
+    }	## end of generation loop 
     
     ## Assign results matrices to lists for multiple runs
     listOut$results[[i]] <- results
     listOut$genotype[[i]] <- genotype
+    listOut$fitness[[i]] <- fitnessOutput( Wniche )    
     
     ## Plots
     if( produce.plots ) plot_outputs_all( listOut=listOut, scen_num=i, savePlots=savePlots)
 
-    
-    
-    ## output fitnesses by niche & genotype
-      
-      fbn <- matrix( ncol=9, nrow=9 )
-      
-      colnames(fbn) <- c("-,-", "a,-", "A,-", "-,b", "-,B", "a,b", "A,B", "A,b", "a,B")
-      #default order is different : 00 a0 A0 0b ab Ab 0B aB AB
-      #potential problems in ordering of rows & cols avoided by filling by name below
-      
-      rownames(fbn) <- c("SS1SS2", "SS1RS2", "SS1RR2",
-                         "RS1SS2", "RS1RS2", "RS1RR2",
-                         "RR1SS2", "RR1RS2", "RR1RR2" )
-
-      for( locus1 in dimnames(Wniche)$locus1)
-      {
-        for( locus2 in dimnames(Wniche)$locus2)
-        {
-          #this is a good way of doing but the columns end in a different order
-          #which wouldn't be a problem except that initially I'm trying
-          #to keep results identical to Beths
-          #fbn[paste0(locus1,locus2),] <- Wniche[locus1,locus2,,]
-          #so instead go through each niche
-          for( niche1 in dimnames(Wniche)$niche1)
-          {
-            for( niche2 in dimnames(Wniche)$niche2)
-            {
-              #get columnName by converting 0 to - and inserting commas
-              columnName <- paste0(niche1,",",niche2)
-              columnName <- gsub('0','-',columnName)
-              #get rowname by pasting
-              rowName <- paste0(locus1,locus2)
-              
-              fbn[rowName,columnName] <- Wniche[locus1,locus2,niche1,niche2]
-            }
-          }
-        }
-      }
-      
-      listOut$fitness[[i]] <- fbn
-      
-
-    
-    
-  }		### end of scenarios loop (each column in input) 
+  }		## end of scenarios loop (each column in input) 
   
   
-  #return list of outputs
+  # return list of outputs
   invisible(listOut)
   
 }
