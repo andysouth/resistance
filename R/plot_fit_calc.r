c7eb92075b05e66a721e03bd4f353bc1a1b99a79
AndySouth <southandy@gmail.com>
2017-02-22 17:35:09 +0000

    paper2 writing

--- R/plot_fit_calc.r
+++ R/plot_fit_calc.r
@@ -2,9 +2,11 @@
 #' 
 #' accepts inputs
 
-#' @param effectiveness
+#' @param effectiveness insecticide effectiveness
 #' @param resistance_restoration
-#' @param dominance
+#' @param dominance_restoration
+#' @param dominance_cost
+#' @param cost cost of resistance
 #' @param title optional title for the plot
 # @param yblank whether to remove y axis title & labels
 # @param size text size for RS labels, default 4
@@ -18,7 +20,7 @@
 #' @return 
 #' @export
 
-plot_fit_calc <- function ( effectiveness=0.8, resistance_restoration=0.5, dominance=0.5, cost=0.3 ){
+plot_fit_calc <- function ( effectiveness=0.8, resistance_restoration=0.5, dominance_restoration=0.5, dominance_cost=0.5, cost=0.3 ){
  
   
   #library(gridExtra)
@@ -31,7 +33,7 @@ plot_fit_calc <- function ( effectiveness=0.8, resistance_restoration=0.5, domin
   # In presence of insecticide
   ss <- 1-effectiveness
   rr <- 1-effectiveness + (effectiveness*resistance_restoration)
-  sr <- 1-effectiveness + (effectiveness*resistance_restoration*dominance)
+  sr <- 1-effectiveness + (effectiveness*resistance_restoration*dominance_restoration)
   
   dfg <- data.frame(x1 = c('SS',  'SR',  'RR'), 
                     x2 = c('SS',  'SR',  'RR'),                    
@@ -43,7 +45,7 @@ plot_fit_calc <- function ( effectiveness=0.8, resistance_restoration=0.5, domin
         #points
         geom_point(size=3, colour='blue') +
         #arrows
-        geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2),size=1, arrow = arrow(length = unit(3,"mm"))) +
+        geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2),size=0.9, arrow = arrow(length = unit(3,"mm"))) +
         #dotted horizontal lines showing calculations
         annotate("segment", x='SS', xend='RR', y=ss, yend=ss, linetype="dotted", colour = "blue") +
         annotate("segment", x='SR', xend='RR', y=rr, yend=rr, linetype="dotted", colour = "blue") +
@@ -57,7 +59,7 @@ plot_fit_calc <- function ( effectiveness=0.8, resistance_restoration=0.5, domin
         # annotate("text", x = 'SR', y = sr+(rr-sr)/2, hjust=0, label = " dominance") + 
         # annotate("text", x = 'RR', y = ss+(rr-ss)/2, hjust=0, label = " resistance\n  restoration") + 
         annotate("label", x = 'SS', y = ss+(1-ss)/2, size=3, label = " effectiveness") +
-        annotate("label", x = 'SR', y = sr+(rr-sr)/2, size=3, label = " dominance") + 
+        annotate("label", x = 'SR', y = sr+(rr-sr)/2, size=3, label = " dominance of\n  restoration") + 
         annotate("label", x = 'RR', y = ss+(rr-ss)/2, size=3, label = " resistance\n  restoration") + 
     
         theme_bw() +
@@ -75,7 +77,7 @@ plot_fit_calc <- function ( effectiveness=0.8, resistance_restoration=0.5, domin
   # insecticide absent
   ss <- 1
   rr <- 1-cost
-  sr <- 1-cost + (1-dominance)*cost  
+  sr <- 1-cost + (1-dominance_cost)*cost  
 
   dfg <- data.frame(x1 = c('SS',  'SR',  'RR'), 
                     x2 = c('SS',  'SR',  'RR'),                    
@@ -89,8 +91,8 @@ plot_fit_calc <- function ( effectiveness=0.8, resistance_restoration=0.5, domin
     #arrows
     #geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2),size=1, arrow = arrow(length = unit(3,"mm"))) +
     #because I don't want an arrow for SS, easier to do with annotate
-    annotate("segment", x='SR', xend='SR', y=rr, yend=sr, size=1, arrow=arrow(length = unit(3,"mm"))) +
-    annotate("segment", x='RR', xend='RR', y=ss, yend=rr, size=1, arrow=arrow(length = unit(3,"mm"))) +
+    annotate("segment", x='SR', xend='SR', y=rr, yend=sr, size=0.9, arrow=arrow(length = unit(3,"mm"))) +
+    annotate("segment", x='RR', xend='RR', y=ss, yend=rr, size=0.9, arrow=arrow(length = unit(3,"mm"))) +
     #dotted horizontal lines showing calculations
     annotate("segment", x='SS', xend='RR', y=ss, yend=ss, linetype="dotted", colour = "blue") +
     annotate("segment", x='SR', xend='RR', y=rr, yend=rr, linetype="dotted", colour = "blue") +
@@ -100,7 +102,7 @@ plot_fit_calc <- function ( effectiveness=0.8, resistance_restoration=0.5, domin
     scale_x_discrete(limits = c('SS',  'SR',  'RR')) +
     #text annotations "label" creates box "text"doesn't
     #annotate("text", x = 'SS', y = ss+(1-ss)/2, hjust=0, label = " effectiveness") +
-    annotate("label", x = 'SR', y = sr+(rr-sr)/2, size=3, label = " dominance") + 
+    annotate("label", x = 'SR', y = sr+(rr-sr)/2, size=3, label = " dominance\n  of cost") + 
     annotate("label", x = 'RR', y = ss+(rr-ss)/2, size=3, label = " resistance\n  cost") + 
     
     theme_bw() +
