230df753f407ffcda65bba37c1f0402277502c7c
AndySouth <southandy@gmail.com>
2017-12-13 20:02:38 +0000

    tidy

--- R/array_named.r
+++ R/array_named.r
@@ -14,7 +14,15 @@
 #'              niche1=c('0','a','A'), niche2=c('0','b','B') )
 #' array_named( sex = c('f','m'), locus1 = c('SS1','RS1','RR1'), 
 #'              locus2 = c('SS2','RS2','RR2'), 
-#'              niche1 = c('0','a','A'), niche2 = c('0','a','A') )#' 
+#'              niche1 = c('0','a','A'), niche2 = c('0','a','A') )#'
+#' # to create arrays with flexible dimension names and numbers
+#' # e.g. here with flexible number of dimensions named niche*
+#' n_insecticides <- 3
+#' # list(c()) is important below to create one list for each dim
+#' l_dimnames <- rep(list(c('no','lo','hi')), n_insecticides)
+#' names(l_dimnames) <- paste0("niche", 1:n_insecticides)
+#' do.call(array_named,l_dimnames)
+#'                
 #' @return array
 #' @export
 
@@ -23,4 +31,12 @@ array_named <- function(...)
    array(0, dim = lengths(list(...)), dimnames = list(...))
   }
 
-
+# 4/10/17 this was a potential way of specifying the names of dimnames separately
+# to try to make the number and names of dimensions flexible
+# but instead solved by do.call as in the examples above
+# array_named_flex <- function(namesdimnames, ...)
+# {
+#   a_ <- array(0, dim = lengths(list(...)), dimnames = list(...))
+#   names(dimnames(a_)) <- namesdimnames
+#   return(a_)
+# }
