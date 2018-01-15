86788035588f67902f175aefe80fbb66383b638d
AndySouth <southandy@gmail.com>
2018-01-15 15:54:51 +0000

    new UI with cost option

---
+++ inst/shiny/resistmixseq/server.r
@@ -0,0 +1,149 @@
+#resistance/shiny/resistmob2/server.r
+#andy south 3/6/16
+#unmodified from MixSeqResist1, only changes in UI
+
+
+library(shiny)
+#library(devtools)
+#install_github('AndySouth/resistance')
+library(resistance)
+
+
+shinyServer(function(input, output) {
+  
+  output$plotDefault <- renderPlot({
+    
+    #isolate reactivity of other objects
+    isolate({
+      runcurtis_f2( )
+    }) #end isolate 
+  })    
+    
+  
+  output$plotA <- renderPlot({
+    
+    #add dependency on the button
+    if ( input$aButtonRunA >= 0 ) 
+    {
+      #isolate reactivity of other objects
+      isolate({
+        
+        #cat("running resistSimple with these inputs:", input$P_1, input$P_2*input$P_1, input$h.RS1_00, input$h.RS2_00,"\n")
+        
+        runcurtis_f2( max_gen = 500,
+                      P_1 = input$frequency_A1, 
+                      P_2 = input$frequency_A2,
+                      #P_2 = input$P_2*input$P_1, #if doing 2 as a proportion of 1, i need to put a limit of 1 on this
+                      h.RS1_A0 = input$dominance_A1, 
+                      h.RS2_0B = input$dominance_A2,
+                      exposure = input$exposure_A,
+                      phi.SS1_A0 = input$effectiveness_A1,
+                      phi.SS2_0B = input$effectiveness_A2,
+
+                      rr_restoration_ins1 = input$advantage_A1,
+                      rr_restoration_ins2 = input$advantage_A2,
+                      
+                      #TODO what is the default dominance of cost
+                      
+                      z.RR1_00 = input$cost_A1,
+                      z.RR2_00 = input$cost_A2,
+                      
+                      h.RS1_00 = input$domcost_A1,
+                      h.RS2_00 = input$domcost_A2,
+                                            
+                      cex.axis = 1,
+                      
+                      #strategyLabels = c('seq','','adapt','mix2'),
+                      
+                      addCombinedStrategy = FALSE )
+        
+                      #correct_mix_deploy = input$correct_mix_deploy,
+                      #addCombinedStrategy = input$addCombinedStrategy )
+        
+        #a hack to output the inputs
+        cat("A:\n")
+        cat("runcurtis_f2( max_gen=500, ", 
+                      "P_1 =",input$frequency_A1,",", 
+                      "P_2 =",input$frequency_A2,",", 
+                      "h.RS1_A0 =",input$dominance_A1,",", 
+                      "h.RS2_0B =",input$dominance_A2,",",
+                      "exposure =",input$exposure_A,",",
+                      "phi.SS1_A0 =",input$effectiveness_A1,",",
+                      "phi.SS2_0B =",input$effectiveness_A2,",",
+                      "rr_restoration_ins1 =",input$advantage_A1,",",
+                      "rr_restoration_ins2 =",input$advantage_A2,",",
+            
+                      "z.RR1_00 =", input$cost_A1,",",
+                      "z.RR2_00 =", input$cost_A2,",",
+            
+                      "addCombinedStrategy = FALSE,", 
+                      "strategyLabels = c('seq','','adapt','mix2')",
+                      ")\n" )
+        
+        #"correct_mix_deploy =",input$correct_mix_deploy,",",
+        #"addCombinedStrategy =",input$addCombinedStrategy, 
+               
+      }) #end isolate  
+    } #end button
+  })
+
+  output$plotB <- renderPlot({
+    
+    #add dependency on the button
+    if ( input$aButtonRunB >= 1 ) 
+    {
+      #isolate reactivity of other objects
+      isolate({
+        
+        runcurtis_f2( max_gen = 500,
+                      P_1 = input$frequency_B1, 
+                      P_2 = input$frequency_B2,
+                      h.RS1_A0 = input$dominance_B1, 
+                      h.RS2_0B = input$dominance_B2,
+                      exposure = input$exposure_B,
+                      phi.SS1_A0 = input$effectiveness_B1,
+                      phi.SS2_0B = input$effectiveness_B2,
+
+                      rr_restoration_ins1 = input$advantage_B1,
+                      rr_restoration_ins2 = input$advantage_B2,
+                      
+                      z.RR1_00 = input$cost_B1,
+                      z.RR2_00 = input$cost_B2,
+                      
+                      h.RS1_00 = input$domcost_A1,
+                      h.RS2_00 = input$domcost_A2,
+                      
+                      cex.axis = 1,
+                      
+                      #strategyLabels = c('seq','','adapt','mix2'),
+                      
+                      addCombinedStrategy = FALSE )
+        #correct_mix_deploy = input$correct_mix_deploy,
+        #addCombinedStrategy = input$addCombinedStrategy )
+        
+        #a hack to output the inputs
+        cat("B:\n")
+        cat("runcurtis_f2( max_gen=500, ", 
+            "P_1 =",input$frequency_B1,",", 
+            "P_2 =",input$frequency_B2,",", 
+            "h.RS1_A0 =",input$dominance_B1,",", 
+            "h.RS2_0B =",input$dominance_B2,",",
+            "exposure =",input$exposure_B,",",
+            "phi.SS1_A0 =",input$effectiveness_B1,",",
+            "phi.SS2_0B =",input$effectiveness_B2,",",
+            "rr_restoration_ins1 =",input$advantage_B1,",",
+            "rr_restoration_ins2 =",input$advantage_B2,",",
+            
+            "z.RR1_00 =", input$cost_B1,",",
+            "z.RR2_00 =", input$cost_B2,",",
+            
+            "addCombinedStrategy = FALSE,", 
+            "strategyLabels = c('seq','','adapt','mix2')",
+            ")\n" )
+        
+      }) #end isolate  
+    } #end button
+  })  
+  
+    
+})
---
+++ inst/shiny/resistmixseq/ui.r
@@ -0,0 +1,147 @@
+#resistance/shiny/resistmixseq/ui.r
+#andy south 12/1/18
+#a mobile compatible app with cost, cited from Malaria Journal paper 2018
+
+library(shiny)
+
+
+shinyUI(fluidPage( theme = "bootstrap_simplex.css",
+#shinyUI(fixedPage( theme = "bootstrap_simplex.css",
+ 
+  #can add CSS controls in here
+  #http://shiny.rstudio.com/articles/css.html
+  #http://www.w3schools.com/css/css_rwd_mediaqueries.asp
+  tags$head(
+    tags$style(HTML("
+
+                    [class*='col-'] {
+                        padding: 5px;
+                        border: 1px;
+                        position: relative;
+                        min-height: 1px;
+                    }
+
+                    .container {
+                      margin-right: 0;
+                      margin-left: 0;
+                      float: left;
+                    }
+                    .col-sm-1 {width: 8.33%; float: left;}
+                    .col-sm-2 {width: 16.66%; float: left;}
+                    .col-sm-3 {width: 25%; float: left;}
+                    .col-sm-4 {width: 33.33%; float: left;}
+                    .col-sm-5 {width: 41.66%; float: left;}
+                    .col-sm-6 {width: 50%;  float: left; padding: 0px;} !to make more space for plots
+                    .col-sm-7 {width: 58.33%; float: left;}
+                    .col-sm-8 {width: 66.66%; float: left;}
+                    .col-sm-9 {width: 75%; float: left;}
+                    .col-sm-10 {width: 83.33%; float: left;}
+                    .col-sm-11 {width: 91.66%; float: left;}
+                    .col-sm-12 {width: 100%; float: left;}
+                    
+                    "))
+    ),  
+     
+  title = "resistance to insecticide mixtures",
+  
+  h6("Development of resistance to insecticides used in mixtures and sequence. Modify inputs and compare 2 scenarios. by @southmapr"),  
+  
+  #fixedRow(
+  fluidRow(
+    #column(1),    
+    column(6, plotOutput('plotA')),
+    column(6, plotOutput('plotB'))
+    #column(1)    
+  ), #end fixed row
+  
+  fluidRow(
+    #column(4, h5("takes a few seconds :")),
+    column(2, p()),
+    column(2, actionButton('aButtonRunA', 'Run Scenario A')),
+    column(1, p()),   
+    column(3, h6("(takes a few seconds)")),
+    column(2, actionButton('aButtonRunB', 'Run Scenario B')),
+    column(2, p())
+  ), #end fixed row
+  
+  
+  hr(),
+  
+  fluidRow(
+    column(2, offset = 1,
+           h5("scenario & insecticide "),
+           h2("A1"),
+           h2("A2"),
+           hr(),
+           h2("B1"),
+           h2("B2")
+    ),
+    column(1, offset = 0,
+           #h5("Starting frequency of resistance"),
+           h5("Start Freq."),
+           #numericInput("P_1", "locus1: 0.01", 0.01, min = 0.0001, max = 0.1, step = 0.001),
+           #numericInput("P_2", "locus2: 0.01", 0.01, min = 0.0001, max = 0.1, step = 0.001)
+           sliderInput("frequency_A1", NULL, val=0.01, min = 0.0001, max = 0.1, step = 0.001, ticks=FALSE),
+           sliderInput("frequency_A2", NULL, val=0.01, min = 0.0001, max = 0.1, step = 0.001, ticks=FALSE),
+           hr(),
+           sliderInput("frequency_B1", NULL, val=0.01, min = 0.0001, max = 0.1, step = 0.001, ticks=FALSE),
+           sliderInput("frequency_B2", NULL, val=0.01, min = 0.0001, max = 0.1, step = 0.001, ticks=FALSE)
+           
+    ),    
+    column(1, offset = 0,
+           h5("Exposure"),
+           hr(),
+           sliderInput("exposure_A", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           hr(),hr(),hr(),
+           sliderInput("exposure_B", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           hr()
+    ),    
+    column(1, offset = 0,
+           h5("Effectiveness"),
+           sliderInput("effectiveness_A1", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           sliderInput("effectiveness_A2", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           hr(),
+           sliderInput("effectiveness_B1", NULL, val=0.9, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           sliderInput("effectiveness_B2", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE)             
+    ),
+    column(1, offset = 0,
+           h5("R. restoration"),
+           sliderInput("advantage_A1", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           sliderInput("advantage_A2", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           hr(),
+           sliderInput("advantage_B1", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           sliderInput("advantage_B2", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE)             
+    ),
+    column(1, offset = 0,
+           h5("Dominance"),
+           sliderInput("dominance_A1", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           sliderInput("dominance_A2", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           hr(),
+           sliderInput("dominance_B1", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           sliderInput("dominance_B2", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE)
+    ),
+    column(1, offset = 0,
+           h5("Cost"),
+           sliderInput("cost_A1", NULL, val=0, min = 0, max = 0.1, step = 0.01, ticks=FALSE),
+           sliderInput("cost_A2", NULL, val=0, min = 0, max = 0.1, step = 0.01, ticks=FALSE),
+           hr(),
+           sliderInput("cost_B1", NULL, val=0, min = 0, max = 0.1, step = 0.01, ticks=FALSE),
+           sliderInput("cost_B2", NULL, val=0, min = 0, max = 0.1, step = 0.01, ticks=FALSE)
+    ),
+    column(1, offset = 0,
+           h5("Dominance of cost"),
+           sliderInput("domcost_A1", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           sliderInput("domcost_A2", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           hr(),
+           sliderInput("domcost_B1", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE),
+           sliderInput("domcost_B2", NULL, val=0.5, min = 0, max = 1, step = 0.1, ticks=FALSE)
+    )
+    
+    # column(2, offset = 0,
+    #        h5("Extra params not included in Curtis"),
+    #        numericInput("correct_mix_deploy", "Mixture correct deployment", 1, min = 0, max = 1, step = 0.1),
+    #        checkboxInput("addCombinedStrategy", "plot adaptive strategy",FALSE)
+    # )
+  ) #end fixed row
+
+))
